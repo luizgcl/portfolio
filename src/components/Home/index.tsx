a5b426e38c15d4a123fe877fec70e6ced77056f3
luizgcl <luiizgcl@gmail.com>
2023-01-16 22:44:49 -0300

    chore: adaptation for mobile

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -13,27 +13,29 @@ export function Home() {
           {'<luizgcl/>'}
         </h1>
         <div 
-          className="mt-28 flex items-center gap-10 ease-in-out h-[50vh]">
+          className="mt-28 flex flex-col md:flex-row items-center gap-10 ease-in-out md:h-[50vh]">
           <Photo/>
           <p 
-            className="font-bold text-gray-200 flex flex-col text-left w-[650px]">
+            className="font-bold text-gray-200 flex flex-col text-left md:w-[650px]">
               <span className="text-3xl default-gradient w-[180px] mb-2">
                 Luiz Leme
               </span>
-              <span className="text-xl">
+              <span className="text-md md:text-xl">
                 20 anos, atualmente estou cursando o 2º período de Sistemas para Internet pela Fatec de Jales.
               </span>
-              <span className="text-xl">
+              <span className="text-md md:text-xl">
                 Desenvolvedor Full Stack, atualmente trabalho na empresa Madelife, onde utilizo as técnologias Angular e Laravel para o desenvolvimento.<br/>
                 Nas horas vagas estudo Node, Elixir, React e ReactNative.
               </span>
           </p>
         </div>
         <footer
-          className="mt-28 flex flex-1 flex-row gap-4 bottom-10 fixed"
+          className="mt-12 md:mt-28 flex flex-1 flex-col md:flex-row gap-4 md:bottom-10 md:fixed"
         >
           <a
             className="p-1 bg-gradient-to-r from-purple-700 to-emerald-500 w-[290px] rounded-md cursor-pointer flex"
+             target={"_blank"}
+            href="https://github.com/luizgcl"
           >
             <span className="flex items-center justify-center px-6 py-2 w-[290px] text-gray-200 bg-gray-700 hover:bg-gradient-to-r">
               <GithubLogo size={64} />
@@ -43,6 +45,8 @@ export function Home() {
 
           <a
             className="p-1 bg-gradient-to-b from-blue-400 to-blue-600 w-[290px] rounded-md cursor-pointer flex"
+            target={"_blank"}
+            href="https://linkedin.com/in/luizgcl"
           >
             <span className="flex items-center justify-center px-6 py-2 w-[290px] text-gray-200 bg-gray-700 hover:bg-gradient-to-b">
               <LinkedinLogo size={64} />
@@ -51,10 +55,12 @@ export function Home() {
           </a>
 
           <a
-            className="p-1 bg-gradient-to-r from-yellow-400 to-pink-600 w-[290px] rounded-md cursor-pointer flex"
+            className="p-1 bg-gradient-to-r from-yellow-400 to-pink-600 w-full md:w-[290px] rounded-md cursor-pointer flex"
+            target={"_blank"}
+            href="https://instagram.com/luizgcl"
           >
             
-            <span className="flex items-center justify-center px-6 py-2 w-[290px] text-gray-200 bg-gray-700 hover:bg-gradient-to-r">
+            <span className="flex items-center justify-center px-6 py-2 w-full md:w-[290px] text-gray-200 bg-gray-700 hover:bg-gradient-to-r">
               <InstagramLogo size={64} />
               <p className="font-bold">Acesse meu Instagram</p>
             </span>
