fa6b34d64b541a389482f9272d9f17340c03c003
luizgcl <luiz.leme@madelife.med.br>
2023-01-31 00:48:12 -0300

    fix: image width

--- src/components/Photo/index.tsx
+++ src/components/Photo/index.tsx
@@ -8,7 +8,7 @@ export function Photo(props: {className: string}) {
       <img
         src='https://i.imgur.com/VV12Gkv_d.png'
         alt="Autor do site"
-        className="rounded-lg my-photo"
+        className="rounded-lg my-photo max-w-100 h-auto"
       />
     </div>
   )
